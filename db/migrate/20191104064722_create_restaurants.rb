4b1790cd50a3908de06455c854a412b4857536b2
Luka <sensanati@gmail.com>
2019-11-04 15:10:03 +0800

    Add 'locations' string to restaurant model
    This will later be automatically converted into a lat/long combo using a GeoCoder gem

--- db/migrate/20191104064722_create_restaurants.rb
+++ db/migrate/20191104064722_create_restaurants.rb
@@ -2,8 +2,9 @@ class CreateRestaurants < ActiveRecord::Migration[5.2]
   def change
     create_table :restaurants do |t|
       t.string :name
-      t.float :long
-      t.float :lat
+      t.string :address
+      t.float :longitude
+      t.float :latitude
       t.string :cuisine
       t.integer :price_level
       t.references :user, foreign_key: true
