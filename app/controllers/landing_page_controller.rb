bf4e55d4a149f53967d8a24e815fce84197511b5
Luka <sensanati@gmail.com>
2019-11-04 16:59:38 +0800

    Create the routes, controllers and the views for the controllers (#3)
    * Add authentication redirect for users for landing page
    
    * Add dashboard controller
    
    * Finish drawing all routes (Big question mark)
    
    * Routes perhaps finished again?????
    
    * Add controllers and respective views

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -4,4 +4,12 @@ class LandingPageController < ApplicationController
   def landing_page
     render layout: false
   end
+
+  def how_it_works
+    render layout: false
+  end
+
+  def pricing
+    render layout: false
+  end
 end
