e82120151af75caa7e7e0561d8a13780a4fc1a02
Josh Unwin <joshunwin@protonmail.com>
2019-11-04 15:55:27 +0800

    added theme css as well as a landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'landing_page#landing_page'
+
+  get '/dashboard', to: 'pages#dashboard'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
