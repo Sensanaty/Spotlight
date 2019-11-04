cd1e5df431de9d559d65275e8bd0d940286728f3
Josh Unwin <joshunwin@protonmail.com>
2019-11-04 15:55:51 +0800

    removed temp routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'landing_page#landing_page'
 
-  get '/dashboard', to: 'pages#dashboard'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
