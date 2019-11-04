5599b82f176a76cc278e6d7eae4916d6b62f1a89
Jeroen Rutten <rutten.jr@gmail.com>
2019-11-04 21:47:34 +0800

    Some components to be continued (#5)
    * Some components to be continued
    
    * Removed unnecessary newline

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
   # Check whether the user is authenticated (logged in) or not, if not then take them to a landing page, otherwise take
   # them to the dashboard page
   authenticated do
     root :to => 'dashboard#home', as: :authenticated
   end
   root :to => 'landing_page#landing_page'
-  
+
   get '/dashboard/feed', to: 'dashboard#feed'
   get '/dashboard/explore', to: 'dashboard#explore'
-  
+
   get '/landing_page/how-it-works', to: 'landing_page#how_it_works'
   get '/landing_page/pricing', to: 'landing_page#pricing'
+  get '/pages/components', to: 'pages#components'
+  get '/pages/dashboard', to: 'pages#dashboard'
 end
