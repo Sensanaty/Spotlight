8bc3652d78d569acf8fdeab9d957cc1303d4a88e
Sensanaty <sensanati@gmail.com>
2019-11-05 17:21:12 +0800

    Add the fetcher service to the landing_page_controller to test it out

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -2,7 +2,7 @@ class LandingPageController < ApplicationController
   skip_before_action :authenticate_user!
 
   def landing_page
-    @location = GoogleFetcherService.new("La Barraca Canggu").grab_place_id
+    @location = GoogleFetcherService.new("La Barraca Canggu").grab_place
   end
 
   def how_it_works
