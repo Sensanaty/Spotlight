355e312845c56034216cb6353c6312790a335c5e
Andrea B <ab85@pm.me>
2019-11-05 16:00:30 +0800

    zomato api completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_05_073419) do
+ActiveRecord::Schema.define(version: 2019_11_05_075626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2019_11_05_073419) do
     t.string "reviewer"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "profile_image"
   end
 
   add_foreign_key "orders", "users"
