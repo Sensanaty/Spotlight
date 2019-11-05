b0a45ec766a079e9a415a8030fe76b3c590df26c
jeroen rutten <rutten.jr@gmail.com>
2019-11-05 18:25:20 +0800

    yelpmodel with first API response data

--- app/models/yelp.rb
+++ app/models/yelp.rb
@@ -7,8 +7,13 @@ require 'pry-byebug'
 
 url = 'https://api.yelp.com/v3/businesses/WavvLdfdP6g8aZTtbBQHTw/reviews'
 
-test = RestClient.get(url, headers={'Authorization': 'Bearer 2MylewUK6xe2HIBTOrHcV_WWkHW5DuUX18WUZuKS_dDMBO27gklYOYlNjDNBIlbhKqcuV3NJTLdiaK9h5CQstBPnWD287DG90nryz-3PWIu-MJh17_zXhCbk8sBiXHYx' })
-byebug
+serialized_reviews = RestClient.get(url, headers={'Authorization': 'Bearer 2MylewUK6xe2HIBTOrHcV_WWkHW5DuUX18WUZuKS_dDMBO27gklYOYlNjDNBIlbhKqcuV3NJTLdiaK9h5CQstBPnWD287DG90nryz-3PWIu-MJh17_zXhCbk8sBiXHYx' })
 
-p test
+reviews = JSON.parse(serialized_reviews)
+
+
+#2 gettherestaurantid
+#3 saveit in an array
+#4 loop through it with review API call
+
 
