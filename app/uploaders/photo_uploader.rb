34c8b9637084597f0edfa944b0da948ab240027f
Josh Unwin <joshunwin@protonmail.com>
2019-11-05 14:20:06 +0800

    added cloudinary and carrierwave, added a photo column to restaurants db table.

---
+++ app/uploaders/photo_uploader.rb
@@ -0,0 +1,5 @@
+# app/uploaders/photo_uploader.rb
+class PhotoUploader < CarrierWave::Uploader::Base
+  include Cloudinary::CarrierWave
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_04_065800) do
+ActiveRecord::Schema.define(version: 2019_11_05_061713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2019_11_04_065800) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
     t.index ["user_id"], name: "index_restaurants_on_user_id"
   end
 
