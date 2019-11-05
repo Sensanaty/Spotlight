a1a34dc4382af066d332daf7527931ce985e8ccd
Sensanaty <sensanati@gmail.com>
2019-11-05 17:20:56 +0800

    Create a fetcher service for the google API to fetch and parse the JSON

--- app/services/google_fetcher_service.rb
+++ app/services/google_fetcher_service.rb
@@ -6,17 +6,12 @@ class GoogleFetcherService
     @name = name
   end
 
-  def grab_place_id
+  def grab_place
     formatted_name = @name.strip.gsub(/\s/, "%20") # All whitespace must be converted into %20 for the API to respond
-    fields = "place_id" # ,name,geometry,formatted_address,rating,user_ratings_total"
-    parsed_id = open("#{ENV['GOOGLE_BASE_URL']}#{formatted_name}&inputtype=textquery&fields=#{fields}&key=#{ENV['GOOGLE_API_KEY']}").read
-    JSON.parse(parsed_id)
-  end
+    parsed_id = open("#{ENV['GOOGLE_BASE_URL']}#{formatted_name}&inputtype=textquery&fields=place_id&key=#{ENV['GOOGLE_API_KEY']}").read
+    returned_place_id = JSON.parse(parsed_id)["candidates"][0]["place_id"]
 
-  def place_details
-    place_id = grab_place_id
-    fields = ""
-    parsed_details = open("#{ENV[GOOGLE_DETAILS_URL]}#{place_id}&fields=#{fields}")
-    https://maps.googleapis.com/maps/api/place/details/json?place_id=
+    fields = "formatted_address,geometry,icon,name,place_id,type,website,rating,review,user_ratings_total,price_level"
+    JSON.parse(open("#{ENV['GOOGLE_DETAILS_URL']}#{returned_place_id}&fields=#{fields}&key=#{ENV['GOOGLE_API_KEY']}").read)
   end
 end
