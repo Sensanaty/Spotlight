22988a24f496d0641b322579c5ebf7f645b9c7c6
Josh Unwin <joshunwin@protonmail.com>
2019-11-05 10:20:21 +0800

    refactored hiding the side bar and top bar when not logged in

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -2,14 +2,11 @@ class LandingPageController < ApplicationController
   skip_before_action :authenticate_user!
 
   def landing_page
-    render layout: false
   end
 
   def how_it_works
-    render layout: false
   end
 
   def pricing
-    render layout: false
   end
 end
