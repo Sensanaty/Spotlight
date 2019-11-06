58bf1b0e81d356c02eb03ebe7ea2b1b8f611df32
Sensanaty <sensanati@gmail.com>
2019-11-06 14:50:35 +0800

    Create a reviews controller - Zomato uncommented for exception catching

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,25 +1,34 @@
-require 'pry-byebug'
 class ReviewsController < ApplicationController
   def initialize(name, location)
     @name = name
     @location = location
   end
 
-  def create_google
-    # @google_reviews = GoogleFetcherService.new(@name).grab_place
-  end
-
-  def create_zomato
-    @zomato_reviews = ZomatoFetcherService.new(@name, @location)
-    @zomato_created = @zomato_reviews.each do |key|
-      ZomatoReview.create(review_title: key["key"]["rating_text"],
-                          review_text: key["key"]["review_text"],
-                          rating: key["key"]["rating"],
-                          review_time: key["key"]["review_time_friendly"],
-                          reviewer: key["key"]["user"]["name"],
-                          profile_image: key["key"]["user"]["profile_image"])
+  def create_google # rubocop:disable Metrics/MethodLength
+    @google_reviews = GoogleFetcherService.new(@name).grab_place
+    @google_created = @google_reviews.each do |key|
+      GoogleReview.create(formatted_address: key["result"]["formatted_address"],
+                          latitude: key["result"]["geometry"]["location"]["lat"],
+                          longitude: key["result"]["geometry"]["location"]["lng"],
+                          name: key["result"]["name"],
+                          rating: key["result"]["rating"].round(2),
+                          reviews: key["result"]["reviews"],
+                          types: key["result"]["types"],
+                          user_ratings_total: key["result"]["user_ratings_total"])
     end
   end
+
+  # def create_zomato
+  #   @zomato_reviews = ZomatoFetcherService.new(@name, @location).grab_place
+  #   @zomato_created = @zomato_reviews.each do |key|
+  #     ZomatoReview.create(review_title: key["key"]["rating_text"],
+  #                         review_text: key["key"]["review_text"],
+  #                         rating: key["key"]["rating"],
+  #                         review_time: key["key"]["review_time_friendly"],
+  #                         reviewer: key["key"]["user"]["name"],
+  #                         profile_image: key["key"]["user"]["profile_image"])
+  #   end
+  # end
 
   def create_yelp
     @yelp_reviews = YelpFetcherService.new(@name, @location).grab_place
