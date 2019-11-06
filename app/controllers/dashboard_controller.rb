61eeb557a3e826d964594b1f7edc0e5ab88c107d
jeroen rutten <rutten.jr@gmail.com>
2019-11-06 16:42:47 +0800

    feedpage code ready to test with actual reviews

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,6 +4,10 @@ class DashboardController < ApplicationController
   end
 
   def feed
+    # @restaurant = Restaurant.find_by(user_id: current_user)
+    # @yelp_reviews = @restaurant.yelp_reviews.all.sort_by(&:rating)
+    # @reviews_google = @restaurant.reviews_google.all
+    # @reviews_zomato = @restaurant.reviews_zomato.all
   end
 
   def explore
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,5 @@
 class Restaurant < ApplicationRecord
   belongs_to :user
   mount_uploader :photo, PhotoUploader
+  #add has_many?
 end
