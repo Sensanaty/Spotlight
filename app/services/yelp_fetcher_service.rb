9b2a24cc53810e2481c48241e119611c09914196
Sensanaty <sensanati@gmail.com>
2019-11-06 14:49:26 +0800

    Finish the Yelp fetcher service

--- app/services/yelp_fetcher_service.rb
+++ app/services/yelp_fetcher_service.rb
@@ -13,7 +13,7 @@ class YelpFetcherService
   id_uri = "https://api.yelp.com/v3/businesses/search?term=#{@name}&location=#{@location}"
   serialized_restaurants = RestClient.get(id_uri, headers = { 'Authorization': "Bearer #{ENV['YELP_API_KEY']}" })
   restaurant_id = JSON.parse(serialized_restaurants)['businesses'][0]['id']
-  
+
   # 2nd API call
   review_uri = "https://api.yelp.com/v3/businesses/#{restaurant_id}/reviews"
   serialized_reviews = RestClient.get(review_uri, headers = { 'Authorization': "Bearer #{ENV['YELP_API_KEY']}" })
