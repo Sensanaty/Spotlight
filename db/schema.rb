eaf2c9d0691e9eae7f89d944f898311c77398cd5
Josh Unwin <joshunwin@protonmail.com>
2019-11-07 15:19:50 +0800

    Add user profile image, finish first version of sign up flow (#61)
    * Added a photo field to User model to upload user profile image. 2- Added logic to topbar display to show profile photo is exist, otherwise default profile image. 3. Made photo optional for both user and restaurant
    
    * Fixed logic for showing restaurant logo/name/spotlight depending on what exists
    
    * A bit OTT.. Shows restaurant name in webapp title when user has made one, otherwise shows spotlight tagline :D
    
    * created spotlight-green class, added the users name to the restaurant creation window
    
    * finished with first version of user sign up flow - will refactor with a new layout/yield later
    
    * Added Google sign in button - NOT functioning yet, can't test on dev/local host without using ngrok or similar to get a dev domain. Will revisit later
    
    * padded out the landing page a bit, temporary

--- db/schema.rb
+++ db/schema.rb
@@ -93,6 +93,7 @@ ActiveRecord::Schema.define(version: 2019_11_07_070630) do
     t.string "facebook_picture_url"
     t.string "token"
     t.datetime "token_expiry"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
