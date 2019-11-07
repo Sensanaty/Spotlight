8ed9d722b20428fe51ae035f2e9695842ff2e064
jeroen rutten <rutten.jr@gmail.com>
2019-11-07 08:42:09 +0800

    Added Yelp reviews to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,6 +25,15 @@ restaurant_info = {
   price_level: 2
 }
 
+yelp_review_info = {
+  user_name: "Johannes",
+  review_timestamp: "19-03-1987",
+  rating: 4,
+  review: "super awesome place",
+  user_image: "https://i.pinimg.com/236x/93/ec/45/93ec4564a02cbbc3b64aad49dbb183f6.jpg",
+  #profile_url: "https://www.google.com"
+}
+
 puts "Creating user - Joe Bloggs, jb@gmail.com"
 new_user = User.new(user_info)
 
@@ -32,6 +41,13 @@ puts "Creating restaurant - Pizza East, assigned to Joe Bloggs"
 new_restaurant = Restaurant.new(restaurant_info)
 new_restaurant.user = new_user
 new_restaurant.remote_photo_url = restaurant_logo_url
+
+puts "Creating yelp_reviews"
+3.times do
+  new_yelp_rating = YelpReview.new(yelp_review_info)
+  new_yelp_rating.restaurant = new_restaurant
+  new_yelp_rating.save
+end
 
 new_user.save
 new_restaurant.save
