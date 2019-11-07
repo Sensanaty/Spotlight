17d4f2b409470d7bfc967e218e63552fa434e751
Sensanaty <sensanati@gmail.com>
2019-11-07 13:56:48 +0800

    Refactor the Google Fetcher to now accept the new schema

--- app/services/google_fetcher_service.rb
+++ app/services/google_fetcher_service.rb
@@ -8,7 +8,7 @@ class GoogleFetcherService
     @name = name
   end
 
-  def grab_place(id) # rubocop:disable Metrics/MethodLength
+  def grab_place(restaurant_id) # rubocop:disable Metrics/MethodLength
     # 1st API call
     formatted_name = @name.strip.gsub(/\s/, "%20") # All whitespace must be converted into %20 for the API to respond
     parsed_id = open("#{ENV['GOOGLE_BASE_URL']}#{formatted_name}&inputtype=textquery&fields=place_id&key=#{ENV['GOOGLE_API_KEY']}").read
@@ -18,16 +18,17 @@ class GoogleFetcherService
     fields = "formatted_address,geometry,icon,name,place_id,type,website,rating,review,user_ratings_total,price_level"
     parsed_places = JSON.parse(open("#{ENV['GOOGLE_DETAILS_URL']}#{returned_place_id}&fields=#{fields}&key=#{ENV['GOOGLE_API_KEY']}").read)
 
-    GoogleReview.create(formatted_address: parsed_places["result"]["formatted_address"],
-                        latitude: parsed_places["result"]["geometry"]["location"]["lat"],
-                        longitude: parsed_places["result"]["geometry"]["location"]["lng"],
-                        name: parsed_places["result"]["name"],
-                        rating: parsed_places["result"]["rating"],
-                        reviews: parsed_places["result"]["reviews"],
-                        types: parsed_places["result"]["types"],
-                        user_ratings_total: parsed_places["result"]["user_ratings_total"],
-                        restaurant_id: id)
+    parsed_places["result"]["reviews"].each do |place|
+      GoogleReview.create(reviewer_image: place["profile_photo_url"],
+                          reviewer_username: place["author_name"],
+                          reviewer_profile_url: place["author_url"],
+                          review_text: place["text"],
+                          rating: place["rating"],
+                          review_time: place["time"],
+                          restaurant_id: restaurant_id)
+    end
   end
+  
 end
 
 # rubocop:enable Metrics/LineLength
