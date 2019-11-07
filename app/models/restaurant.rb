acd6dfcea8cb05cf9b17fef2e7f510a4edf0a666
Sensanaty <sensanati@gmail.com>
2019-11-07 11:35:30 +0800

    Add proper references to the restaurant model for zomato and yelp

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -7,4 +7,6 @@ class Restaurant < ApplicationRecord
   validates :price_level, inclusion: { in: [1, 2, 3] }
   belongs_to :user
   has_many :google_reviews
+  has_many :yelp_reviews
+  has_many :zomato_reviews
 end
