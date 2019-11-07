6d746db339ffb45f055da3cc81a67514c1350dce
Luka <sensanati@gmail.com>
2019-11-07 12:33:33 +0800

    Update 20191107032356_create_restaurant_reviews.rb

--- db/migrate/20191107032356_create_restaurant_reviews.rb
+++ db/migrate/20191107032356_create_restaurant_reviews.rb
@@ -3,7 +3,7 @@ class CreateRestaurantReviews < ActiveRecord::Migration[5.2]
     create_table :restaurant_reviews do |t|
       t.references :restaurant, foreign_key: true
       t.references :review, polymorphic: true, index: true
-      t.date :date
+      t.datetime :date
       t.timestamps
     end
   end
