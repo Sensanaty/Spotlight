318924f38006f39f2245676e5432e7b083e82b36
Sensanaty <sensanati@gmail.com>
2019-11-07 15:10:20 +0800

    Refactor Google and Yelp fetchers to work with new schema

--- app/services/google_fetcher_service.rb
+++ app/services/google_fetcher_service.rb
@@ -8,7 +8,7 @@ class GoogleFetcherService
     @name = name
   end
 
-  def grab_place(restaurant_id) # rubocop:disable Metrics/MethodLength
+  def grab_place(relate_restaurant_id) # rubocop:disable Metrics/MethodLength
     # 1st API call
     formatted_name = @name.strip.gsub(/\s/, "%20") # All whitespace must be converted into %20 for the API to respond
     parsed_id = open("#{ENV['GOOGLE_BASE_URL']}#{formatted_name}&inputtype=textquery&fields=place_id&key=#{ENV['GOOGLE_API_KEY']}").read
@@ -18,17 +18,17 @@ class GoogleFetcherService
     fields = "formatted_address,geometry,icon,name,place_id,type,website,rating,review,user_ratings_total,price_level"
     parsed_places = JSON.parse(open("#{ENV['GOOGLE_DETAILS_URL']}#{returned_place_id}&fields=#{fields}&key=#{ENV['GOOGLE_API_KEY']}").read)
 
-    parsed_places["result"]["reviews"].each do |place|
-      GoogleReview.create(reviewer_image: place["profile_photo_url"],
-                          reviewer_username: place["author_name"],
-                          reviewer_profile_url: place["author_url"],
-                          review_text: place["text"],
-                          rating: place["rating"],
-                          review_time: place["time"],
-                          restaurant_id: restaurant_id)
+    parsed_places["result"]["reviews"].each do |review|
+      GoogleReview.create(reviewer_image: review["profile_photo_url"],
+                          reviewer_username: review["author_name"],
+                          reviewer_profile_url: review["author_url"],
+                          review_text: review["text"],
+                          rating: review["rating"],
+                          review_time: review["time"],
+                          restaurant_id: relate_restaurant_id)
     end
   end
-  
+
 end
 
 # rubocop:enable Metrics/LineLength
