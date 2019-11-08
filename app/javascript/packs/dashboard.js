87961945270e3c780b4e4115330828c07096909f
Josh Unwin <joshunwin@protonmail.com>
2019-11-08 11:22:28 +0800

    Added new logo to our site :)

---
+++ app/javascript/packs/dashboard.js
@@ -0,0 +1,12 @@
+const zomatoInnerCard = document.querySelector('#add-channel-inner-zomato')
+const tripAdvisorInnerCard = document.querySelector('#add-channel-inner-tripadvisor')
+const yelpInnerCard = document.querySelector('#add-channel-inner-yelp')
+const facebookInnerCard = document.querySelector('#add-channel-inner-facebook')
+const instagramInnerCard = document.querySelector('#add-channel-inner-instagram')
+
+const zomatoFrontCard = document.querySelector('#add-channel-front-zomato')
+const tripAdvisorFrontCard = document.querySelector('#add-channel-front-tripadvisor')
+const yelpFrontCard = document.querySelector('#add-channel-front-yelp')
+const facebookFrontCard = document.querySelector('#add-channel-front-facebook')
+const instagramFrontCard = document.querySelector('#add-channel-front-instagram')
+
