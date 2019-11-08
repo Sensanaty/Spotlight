56989425e2a81be77c374535e19501e5504cf2bc
Sensanaty <sensanati@gmail.com>
2019-11-08 12:19:10 +0800

    Fetch the lat/long of a restaurant from the google autocomplete

--- app/javascript/packs/google_places.js
+++ app/javascript/packs/google_places.js
@@ -3,26 +3,30 @@ function autocomplete() {
     name: 'restaurant_name',
     formatted_address: 'restaurant_address',
     price_level: 'restaurant_price_level',
-  };
+};
 
   document.addEventListener("DOMContentLoaded", function() {
     var restaurantLocatorDisplay = document.getElementById('restaurant_selector');
 
     if (restaurantLocatorDisplay) {
       var autocomplete = new google.maps.places.Autocomplete(restaurantLocatorDisplay, { types: [ 'establishment' ] });
-      autocomplete.setFields(['name', 'formatted_address', 'price_level']);
+      autocomplete.setFields(['name', 'formatted_address', 'price_level', 'geometry']);
       autocomplete.addListener('place_changed', function(e) {
         // if (e.key === "Enter") {
         //   e.preventDefault(); // Do not submit the form on Enter.
         // }
         var place = autocomplete.getPlace();
-        console.log(place);
+        var lat = place.geometry.viewport.ka.g;
+        var lon = place.geometry.viewport.pa.g;
 
         for (var component in componentForm) {
-          console.log(componentForm[component]);
           document.getElementById(componentForm[component]).value = place[component];
           document.getElementById(componentForm[component]).disabled = false;
         }
+        document.getElementById("restaurant_latitude").value = lat;
+        document.getElementById("restaurant_latitude").disabled = false;
+        document.getElementById("restaurant_longitude").value = lon;
+        document.getElementById("restaurant_longitude").disabled = false;
       });
     };
   });
