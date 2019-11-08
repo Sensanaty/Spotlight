60d8f9eef9ce8e0a59bd4ae28e992d14cdf06265
jeroen rutten <rutten.jr@gmail.com>
2019-11-08 12:57:34 +0800

    changed restaurant model to many FS reviews

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -9,4 +9,5 @@ class Restaurant < ApplicationRecord
   has_many :restaurant_reviews
   has_many :google_reviews, through: :restaurant_reviews, source: :review, source_type: 'GoogleReview'
   has_many :yelp_reviews, through: :restaurant_reviews, source: :review, source_type: 'YelpReview'
+  has_many :four_square_reviews, through: :restaurant_reviews, source: :review, source_type: 'FourSquareReview'
 end
