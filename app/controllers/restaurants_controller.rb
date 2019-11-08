55c358dc3d1c8e3380aba7d55f83405445a09d60
Sensanaty <sensanati@gmail.com>
2019-11-08 15:56:24 +0800

    fetch google and yelp reviews on click

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -12,6 +12,7 @@ class RestaurantsController < ApplicationController
 
     if @restaurant.save!
       redirect_to payment_users_path
+      GoogleFetcherService.new(Restaurant.last.name).grab_place(Restaurant.last.id)
     else
       render :new
     end
@@ -21,7 +22,8 @@ class RestaurantsController < ApplicationController
   end
 
   def find_yelp_restaurant
-    puts "yelp"
+    puts "Fetching Yelp Restaurants"
+    YelpFetcherService.new(Restaurant.last.longitude, Restaurant.last.latitude).grab_place(Restaurant.last.id)
   end
 
   def find_zomato_restaurant
