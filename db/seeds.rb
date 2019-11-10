e83b0d6e5c1f32280b52a247bcfaea6488dde9cb
Josh Unwin <joshunwin@protonmail.com>
2019-11-11 07:54:35 +0800

    seed now adds linked_channel' for each successfully added channel in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ restaurant_info = {
   price_level: 2,
   latitude: 40.7085126197085,
   longitude: -73.9638020302915,
-  linked_channels: ['Google']
+  linked_channels: = [],
 }
 
 puts "\e[34mCreating user - Joe Bloggs, jb@gmail.com\e[0m"
@@ -33,19 +33,23 @@ new_user.save
 new_restaurant.save
 
 puts "\n\e[34mFetching from Google\e[0m"
-GoogleFetcherService.new(new_restaurant.name).grab_place(new_restaurant.id)
+google_fetch? = GoogleFetcherService.new(new_restaurant.name).grab_place(new_restaurant.id)
+new_restaurant.linked_channels.push('Google') if google_fetch?
 puts "\e[92mGoogle Fetched!\e[0m"
 
 puts "\n\e[34mFetching from Yelp\e[0m"
 YelpFetcherService.new(new_restaurant.latitude, new_restaurant.longitude).grab_place(new_restaurant.id)
+new_restaurant.linked_channels.push('Yelp')
 puts "\e[92mYelp Fetched!\e[0m"
 
 puts "\n\e[34mFetching from Zomato\e[0m"
 ZomatoFetcherService.new(new_restaurant.name, new_restaurant.latitude, new_restaurant.longitude).grab_place(new_restaurant.id)
+new_restaurant.linked_channels.push('Zomato')
 puts "\e[92mZomato Fetched!\e[0m"
 
 puts "\n\e[34mFetching from FourSquare\e[0m"
 FoursquareFetcherService.new(new_restaurant.name, new_restaurant.latitude, new_restaurant.longitude).grab_place(new_restaurant.id)
+new_restaurant.linked_channels.push('Foursquare')
 puts "\e[92mFourSquare Fetched!\e[0m"
 
 puts "\n\e[32m+=+=+=+=+=+=+=+=+=+=+=+=+=+\e[0m"
