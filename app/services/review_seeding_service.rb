1b73e5d15504656b989ee97bf9881945bc70c616
Sensanaty <sensanati@gmail.com>
2019-11-11 21:57:01 +0800

    Add Squarespace to seed service

--- app/services/review_seeding_service.rb
+++ app/services/review_seeding_service.rb
@@ -51,6 +51,21 @@ class ReviewSeedingService
     end
     puts "\e[92Zomato Seeded\e[0m\n"
   end
+
+  def self.squarespace_seed
+    puts "\n\e[34mNow seeding randomly generated SquareSpace reviews\e[0m"
+    rand(20..90).times do
+      username = "#{Faker::Verb.past_participle}#{Faker::NatoPhoneticAlphabet.code_word}#{rand(1050)}"
+      ZomatoReview.create(reviewer_image: Faker::Avatar.image,
+                          reviewer_username: username,
+                          reviewer_profile_url: "https://sp0tlight.herokuapp.com",
+                          review_text: "#{Faker::Quote.matz} #{Faker::Lorem.sentence(word_count: rand(10..19))}",
+                          rating: rand(1.0..5.0),
+                          review_time: rand(1_420_070_400..1_573_506_000),
+                          restaurant_id: @restaurant.id)
+    end
+    puts "\e[92Squarespace Seeded\e[0m\n"
+  end
 end
 
 # rubocop:enable Metrics/LineLength
