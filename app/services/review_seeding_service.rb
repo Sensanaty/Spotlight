54da455918d07be73f9105a453e24797592a4377
Sensanaty <sensanati@gmail.com>
2019-11-11 21:56:23 +0800

    Add Zomato reviews to the Seed fetcher

--- app/services/review_seeding_service.rb
+++ app/services/review_seeding_service.rb
@@ -36,6 +36,21 @@ class ReviewSeedingService
     end
     puts "\e[92Yelp Seeded\e[0m\n"
   end
+
+  def self.zomato_seed
+    puts "\n\e[34mNow seeding randomly generated Zomato reviews\e[0m"
+    rand(30..60).times do
+      username = "#{Faker::GreekPhilosophers.name}#{Faker::Verb.past_participle}#{Faker::NatoPhoneticAlphabet.code_word}#{rand(1050)}"
+      ZomatoReview.create(reviewer_image: Faker::Avatar.image,
+                          reviewer_username: username,
+                          reviewer_profile_url: "https://sp0tlight.herokuapp.com",
+                          review_text: "#{Faker::Quote.matz} #{Faker::Lorem.sentence(word_count: rand(17..27))}",
+                          rating: rand(1.0..5.0),
+                          review_time: rand(1_420_070_400..1_573_506_000),
+                          restaurant_id: @restaurant.id)
+    end
+    puts "\e[92Zomato Seeded\e[0m\n"
+  end
 end
 
 # rubocop:enable Metrics/LineLength
