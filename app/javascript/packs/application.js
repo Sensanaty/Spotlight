809c42c466eee18c6dc87943a22e7eade2c5cc9a
jeroen rutten <rutten.jr@gmail.com>
2019-11-11 12:34:51 +0800

    cleaned up some more code

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,7 +5,7 @@ import { autocomplete } from "./google_places.js";
 autocomplete();
 
 
-
+// This makes sure the dropdown of filters doesnt colapse once you click on something
 if (document.querySelector('.filter-dropdown-menu')){
   $(".filter-dropdown-menu").click(function(e){
    e.stopPropagation();
