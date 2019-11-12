ed6f227abff78ae2df84541d6beb21fd64c0ba17
jeroen rutten <rutten.jr@gmail.com>
2019-11-12 10:29:01 +0800

    added trackable to user

--- db/schema.rb
+++ db/schema.rb
@@ -130,6 +130,8 @@ ActiveRecord::Schema.define(version: 2019_11_12_011514) do
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
