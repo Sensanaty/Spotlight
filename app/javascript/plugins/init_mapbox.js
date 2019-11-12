bb5e40a0f05e20ce7170f53547a74cb83712acf2
jeroen rutten <rutten.jr@gmail.com>
2019-11-12 16:59:03 +0800

    map sticking

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -41,7 +41,7 @@ const addMarkersToMap = (map, markers) => {
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { duration: 1200,padding: 70, maxZoom: 15 });
+  map.fitBounds(bounds, { duration: 1200,padding: 80, maxZoom: 15 });
 };
 
 const initMapbox = () => {
