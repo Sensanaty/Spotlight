267f7131ca1dfa93d634fb579a18c121386a3c10
Jeroen Rutten <rutten.jr@gmail.com>
2019-11-12 12:04:03 +0800

    Update google_fetcher_service.rb

--- app/services/google_fetcher_service.rb
+++ app/services/google_fetcher_service.rb
@@ -31,7 +31,7 @@ class GoogleFetcherService
                           reviewer_profile_url: review["author_url"],
                           review_text: review["text"],
                           rating: review["rating"],
-                          review_time: DateTime.strptime(parsed_places["result"]["reviews"][0]["time"].to_s, "%s"),
+                          review_time: DateTime.strptime(review["time"].to_s, "%s"),
                           restaurant_id: restaurant.id)
     end
   end
