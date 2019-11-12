4197811556fd4575cc01b735d899bf06350fd371
Sensanaty <sensanati@gmail.com>
2019-11-12 09:59:09 +0800

    Make all the methods take in the Restaurant model and use activerecord

--- app/services/review_seeding_service.rb
+++ app/services/review_seeding_service.rb
@@ -4,65 +4,58 @@
 require 'faker'
 
 class ReviewSeedingService
-  # @restaurant = Restaurant.find_by(user_id: current_user)
-  @restaurant = Restaurant.last
-
-  def self.google_seed
+  def self.google_seed(restaurant)
     puts "\n\e[34mNow seeding randomly generated Google reviews\e[0m"
     rand(50..110).times do
-      username = "#{Faker::Verb.past_participle}#{Faker::GreekPhilosophers.name}#{Faker::NatoPhoneticAlphabet.code_word}#{rand(1050)}"
       GoogleReview.create(reviewer_image: Faker::Avatar.image,
-                          reviewer_username: username,
+                          reviewer_username: Faker::Name.name,
                           reviewer_profile_url: "https://sp0tlight.herokuapp.com",
                           review_text: "#{Faker::Quote.matz} #{Faker::Lorem.sentence(word_count: rand(15..25))}",
                           rating: rand(1.0..5.0),
                           review_time: rand(1_420_070_400..1_573_506_000),
-                          restaurant_id: @restaurant.id)
+                          restaurant_id: restaurant.id)
     end
     puts "\e[92Google Seeded\e[0m\n"
   end
 
-  def self.yelp_seed
+  def self.yelp_seed(restaurant)
     puts "\n\e[34mNow seeding randomly generated Yelp reviews\e[0m"
     rand(50..80).times do
-      username = "#{Faker::Verb.past_participle}#{Faker::NatoPhoneticAlphabet.code_word}#{Faker::GreekPhilosophers.name}#{rand(1050)}"
       YelpReview.create(reviewer_image: Faker::Avatar.image,
-                        reviewer_username: username,
+                        reviewer_username: Faker::Name.name,
                         reviewer_profile_url: "https://sp0tlight.herokuapp.com",
                         review_text: "#{Faker::Quote.matz} #{Faker::Lorem.sentence(word_count: rand(11..23))}",
                         rating: rand(1.0..5.0),
                         review_time: rand(1_420_070_400..1_573_506_000),
-                        restaurant_id: @restaurant.id)
+                        restaurant_id: restaurant.id)
     end
     puts "\e[92Yelp Seeded\e[0m\n"
   end
 
-  def self.zomato_seed
+  def self.zomato_seed(restaurant)
     puts "\n\e[34mNow seeding randomly generated Zomato reviews\e[0m"
     rand(30..60).times do
-      username = "#{Faker::GreekPhilosophers.name}#{Faker::Verb.past_participle}#{Faker::NatoPhoneticAlphabet.code_word}#{rand(1050)}"
       ZomatoReview.create(reviewer_image: Faker::Avatar.image,
-                          reviewer_username: username,
+                          reviewer_username: Faker::Name.name,
                           reviewer_profile_url: "https://sp0tlight.herokuapp.com",
                           review_text: "#{Faker::Quote.matz} #{Faker::Lorem.sentence(word_count: rand(17..27))}",
                           rating: rand(1.0..5.0),
                           review_time: rand(1_420_070_400..1_573_506_000),
-                          restaurant_id: @restaurant.id)
+                          restaurant_id: restaurant.id)
     end
     puts "\e[92Zomato Seeded\e[0m\n"
   end
 
-  def self.squarespace_seed
+  def self.foursquare_seed(restaurant)
     puts "\n\e[34mNow seeding randomly generated SquareSpace reviews\e[0m"
     rand(20..90).times do
-      username = "#{Faker::Verb.past_participle}#{Faker::NatoPhoneticAlphabet.code_word}#{rand(1050)}"
-      ZomatoReview.create(reviewer_image: Faker::Avatar.image,
-                          reviewer_username: username,
-                          reviewer_profile_url: "https://sp0tlight.herokuapp.com",
-                          review_text: "#{Faker::Quote.matz} #{Faker::Lorem.sentence(word_count: rand(10..19))}",
-                          rating: rand(1.0..5.0),
-                          review_time: rand(1_420_070_400..1_573_506_000),
-                          restaurant_id: @restaurant.id)
+      FoursquareReview.create(reviewer_image: Faker::Avatar.image,
+                              reviewer_username: Faker::Name.name,
+                              reviewer_profile_url: "https://sp0tlight.herokuapp.com",
+                              review_text: "#{Faker::Quote.matz} #{Faker::Lorem.sentence(word_count: rand(10..19))}",
+                              rating: rand(1.0..5.0),
+                              review_time: rand(1_420_070_400..1_573_506_000),
+                              restaurant_id: restaurant.id)
     end
     puts "\e[92Squarespace Seeded\e[0m\n"
   end
