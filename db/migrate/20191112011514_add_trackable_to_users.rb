5b4ebcf84419aa745fb04dbe7b0cd70de38112a6
jeroen rutten <rutten.jr@gmail.com>
2019-11-12 09:33:57 +0800

    added trackable to user in migration

--- db/migrate/20191112011514_add_trackable_to_users.rb
+++ db/migrate/20191112011514_add_trackable_to_users.rb
@@ -1,4 +1,7 @@
 class AddTrackableToUsers < ActiveRecord::Migration[5.2]
   def change
+    add_column :users, :sign_in_count, :integer, default: 0, null: false
+    add_column :users, :current_sign_in_at, :datetime
+    add_column :users, :last_sign_in_at, :datetime
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_11_045401) do
+ActiveRecord::Schema.define(version: 2019_11_12_011514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -127,6 +127,9 @@ ActiveRecord::Schema.define(version: 2019_11_11_045401) do
     t.string "token"
     t.datetime "token_expiry"
     t.string "photo"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
