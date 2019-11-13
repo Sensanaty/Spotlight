a0d4f46a13ca39f70fe28190e1d343745927129f
Jeroen Rutten <rutten.jr@gmail.com>
2019-11-13 16:09:37 +0800

    Map cards (#106)
    * started to make cards dynamic
    
    * reworked the map page
    
    * reworked the map page
    
    * added half stars logic
    
    * added half stars logic to competitors
    
    * added code to avoid showing own restaurant as competition
    
    * added cuisine to map cards
    
    * front end changes added home card
    
    * imported the images and redesigned the cards
    
    * formatted cards on map
    
    * front-end competitor cards done
    
    * added photo_reference to restaurant table
    
    * added the image to the card
    
    * Update dashboard_controller.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_13_041415) do
+ActiveRecord::Schema.define(version: 2019_11_13_054747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,6 +92,7 @@ ActiveRecord::Schema.define(version: 2019_11_13_041415) do
     t.text "foursquare_average_rating"
     t.text "tripadvisor_review_count"
     t.text "tripadvisor_average_rating"
+    t.string "photo_reference"
     t.index ["user_id"], name: "index_restaurants_on_user_id"
   end
 
