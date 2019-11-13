d3b642c4ad719dd64801de67192f2c9cdb6b7732
Andrea B <ab85@pm.me>
2019-11-13 10:36:14 +0800

    fixed a typo in the migration

--- db/schema.rb
+++ db/schema.rb
@@ -95,7 +95,7 @@ ActiveRecord::Schema.define(version: 2019_11_12_061339) do
     t.index ["user_id"], name: "index_restaurants_on_user_id"
   end
 
-  create_table "tripadvior_reviews", force: :cascade do |t|
+  create_table "tripadvisor_reviews", force: :cascade do |t|
     t.string "reviewer_image"
     t.string "reviewer_username"
     t.string "reviewer_profile_url"
@@ -105,7 +105,7 @@ ActiveRecord::Schema.define(version: 2019_11_12_061339) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "review_time"
-    t.index ["restaurant_id"], name: "index_tripadvior_reviews_on_restaurant_id"
+    t.index ["restaurant_id"], name: "index_tripadvisor_reviews_on_restaurant_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -167,7 +167,7 @@ ActiveRecord::Schema.define(version: 2019_11_12_061339) do
   add_foreign_key "orders", "users"
   add_foreign_key "restaurant_reviews", "restaurants"
   add_foreign_key "restaurants", "users"
-  add_foreign_key "tripadvior_reviews", "restaurants"
+  add_foreign_key "tripadvisor_reviews", "restaurants"
   add_foreign_key "yelp_reviews", "restaurants"
   add_foreign_key "zomato_reviews", "restaurants"
 end
