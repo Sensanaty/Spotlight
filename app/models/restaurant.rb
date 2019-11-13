8f98cbcb674de6739581d3cfaaa229c30215823a
Josh Unwin <joshunwin@protonmail.com>
2019-11-13 11:33:53 +0800

    1. Renamed chart js files to use lower_snake_case. 2. Updated dashboard weekly summary section to use real data. 3. Updated dashboard card components to look at work better

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -57,4 +57,51 @@ class Restaurant < ApplicationRecord
     end
     (all_ratings.sum / all_ratings.length).round(1) unless all_ratings == []
   end
+
+  def week_name(num_weeks_ago) # This takes a negative integer. EG -2 is the week before this week.
+    self.restaurant_reviews.group_by_week(:review_time, format: "%d " "%b %Y").count.to_a[num_weeks_ago][0]
+  end
+
+  def week_reviews_average(num_weeks_ago)   # This takes a negative integer. EG -2 is the week before this week.
+    reviews = self.restaurant_reviews.group_by_week(&:review_time).to_a[num_weeks_ago][1]
+    ratings = []
+    reviews.each do |r|
+      ratings << r.rating
+    end
+    (ratings.sum / ratings.length).round(1) unless ratings == []
+  end
+
+  def week_reviews_count(num_weeks_ago) # This takes a negative integer. EG -2 is the week before this week.
+    self.restaurant_reviews.group_by_week(:review_time, format: "%d " "%b %Y").count.to_a[num_weeks_ago][1]
+  end
+
+  def week_review_count_comparision(num_weeks_ago_one, num_weeks_ago_two) # Provide two weeks using negative numbers (weeks since current week) to compare
+    week_reviews_count_one = week_reviews_count(num_weeks_ago_one)
+    week_reviews_count_two = week_reviews_count(num_weeks_ago_two)
+
+    if week_reviews_count_one > week_reviews_count_two
+      difference1 = week_reviews_count_one - week_reviews_count_two
+      "#{difference1.to_s} more review#{"s" unless difference1 == 1} than the previous week"
+    elsif week_reviews_count_one < week_reviews_count_two
+      difference1 = week_reviews_count_two - week_reviews_count_one
+      "#{difference1.to_s} less review#{"s" unless difference1 == 1} than the previous week"
+    elsif week_reviews_count_one == week_reviews_count_two
+      "The same number of reviews as the previous week"
+    end
+  end
+
+  def week_review_average_comparision(num_weeks_ago_one, num_weeks_ago_two) # Provide two weeks using negative numbers (weeks since current week) to compare
+    week_reviews_average_one = week_reviews_average(num_weeks_ago_one)
+    week_reviews_average_two = week_reviews_average(num_weeks_ago_two)
+
+    if week_reviews_average_one > week_reviews_average_two
+      difference2 = week_reviews_average_one - week_reviews_average_two
+      "Average rating was #{difference2.to_s} better compared to the previous week"
+    elsif week_reviews_average_one < week_reviews_average_two
+      difference2 = week_reviews_average_two - week_reviews_average_one
+      "Average rating was #{difference2.to_s} worse compared to the previous week"
+    elsif week_reviews_average_one == week_reviews_average_two
+      "Your average rating was the same as last week."
+    end
+  end
 end
--- app/models/restaurant_review.rb
+++ app/models/restaurant_review.rb
@@ -3,5 +3,5 @@ class RestaurantReview < ApplicationRecord
   belongs_to :review, polymorphic: true
 
   include PgSearch::Model
-  pg_search_scope :search_by_rating, against: [:rating]
+pg_search_scope :search_by_rating, against: [:rating]
 end
