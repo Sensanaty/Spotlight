6532945f6266ca68624eba08354c38970f292fb2
Andrea B <ab85@pm.me>
2019-11-14 12:46:13 +0800

    42 answered Deep Thought

--- app/services/tripadvisor_scraper_service.rb
+++ app/services/tripadvisor_scraper_service.rb
@@ -8,9 +8,8 @@ require_relative '../models/tripadvisor_review'
 
 class TripadvisorScraperService
 
-  def self.first_scrape
-    acquire_all_review_urls
-    pages.each do |url|
+  def self.first_scrape(restaurant)
+    TripadvisorScraperService.acquire_all_review_urls(restaurant).each do |url|
       scrape_tripadvisor(url, restaurant)
     end
   end
@@ -24,30 +23,38 @@ class TripadvisorScraperService
   def self.scrape_tripadvisor(url, restaurant)
     browser = Watir::Browser.new :chrome, headless: true
     browser.goto(url)
-    browser.span(text: "More").click
-    reviews = browser.div(id: "REVIEWS").html
-    parsed_reviews = Nokogiri.parse(reviews)
-    10.times do |index|
-      remote_id = parsed_reviews.search('.review-container')[index].attributes['data-reviewid'].value.to_i
-      TripadvisorReview.create(review_time: parsed_reviews.search('.ratingDate')[index].attributes['title'],
-                               rating: parsed_reviews.search('.ui_column.is-9')[index].children.first.attributes['class'].value[-2].to_i,
-                               reviewer_username: parsed_reviews.search('.info_text.pointer_cursor > div')[index].text,
-                               review_text: parsed_reviews.search('.partial_entry')[index].text,
-                               reviewer_image: parsed_reviews.search('.ui_avatar > img')[index].attributes['src'],
-                               remote_id: remote_id,
-                               restaurant_id: restaurant.id)
-    end
+    begin
+        browser.span(text: "More").click
+        reviews = browser.div(id: "REVIEWS").html
+        parsed_reviews = Nokogiri.parse(reviews)
+        10.times do |index|
+          remote_id = parsed_reviews.search('.review-container')[index].attributes['data-reviewid'].value.to_i
+          TripadvisorReview.create(review_time: parsed_reviews.search('.ratingDate')[index].attributes['title'],
+                                   rating: parsed_reviews.search('.ui_column.is-9')[index].children.first.attributes['class'].value[-2].to_i,
+                                   reviewer_username: parsed_reviews.search('.info_text.pointer_cursor > div')[index].text,
+                                   review_text: parsed_reviews.search('.partial_entry')[index].text,
+                                   reviewer_image: parsed_reviews.search('.ui_avatar > img')[index].attributes['src'],
+                                   remote_id: remote_id,
+                                   restaurant_id: restaurant.id)
+        end
+      rescue
+        redirect_to dashboard_path
+      end
   end
 
 
-  def self.acquire_all_review_urls
+  def self.acquire_all_review_urls(restaurant)
     # url here is the first page of the restaurant, which contains reviews, starting from the first ten
     pages = []
-    url = restaurants.tripadvisor_url
+    url = restaurant.tripadvisor_url
     serialized = open(url).read
     parsed = Nokogiri.parse(serialized)
-
+    finished = false
     6.times do |index|
+      if parsed.search('.pageNumbers > a')[index].nil?
+        finished = true
+        break
+      end
       next unless parsed.search('.pageNumbers > a')[index].attributes['href'] != nil
 
       next_page = parsed.search('.pageNumbers > a')[index].attributes['href'].value
@@ -55,7 +62,7 @@ class TripadvisorScraperService
       pages << new_url
     end
 
-    finished = false
+
 
     until finished do
       url = pages[-1]
