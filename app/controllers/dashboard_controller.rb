2a113d1fc349ccb4bfc6be288c88b12e4b53db36
Sensanaty <sensanati@gmail.com>
2019-11-14 10:45:04 +0800

    Push basic refresh button, TODO is styling it to be centered

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -10,9 +10,7 @@ class DashboardController < ApplicationController
     @restaurant = Restaurant.find_by(user_id: current_user)
     @review_types = @restaurant.restaurant_reviews.pluck(:review_type).uniq
     @all_reviews = @restaurant.restaurant_reviews
-    if params[:rating].present?
-      @all_reviews = @all_reviews.where(rating: params[:rating])
-    end
+    @all_reviews = @all_reviews.where(rating: params[:rating]) if params[:rating].present?
     # Leave in code -> if we have time we use this to refactor the way we pull the reviews. Now we use CSS which pulls all.
     # if params[:type].present?
     #   @all_reviews = @all_reviews.where(review_type: params[:type])
@@ -29,8 +27,15 @@ class DashboardController < ApplicationController
     @my_resto = [{ lat: @restaurant.latitude, lng: @restaurant.longitude, image_url: helpers.asset_url('homeicon.png'), infoWindow: render_to_string(partial: "restaurants/info_window", locals: { restaurant: @restaurant })} ]
   end
 
-  def refresh_reviews
-    # code here
+  def refresh_review
+    @restaurant = Restaurant.find_by(user_id: current_user)
+    unless @restaurant.linked_channels.nil?
+      GoogleFetcherService.new.grab_reviews(@restaurant) if @restaurant.linked_channels.include? 'Google'
+      YelpFetcherService.new.grab_reviews(@restaurant) if @restaurant.linked_channels.include? 'Yelp'
+      ZomatoFetcherService.new.grab_reviews(@restaurant) if @restaurant.linked_channels.include? 'Zomato'
+      FoursquareFetcherService.new.grab_reviews(@restaurant) if @restaurant.linked_channels.include? 'Foursquare'
+    end
+    redirect_to dashboard_feed_path
   end
 end
 
